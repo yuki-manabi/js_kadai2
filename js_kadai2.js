87c6f248b633bfab03bbefe74f88a8a5b77d7dab
yuki-manabi <hy.invest1001@gmail.com>
2021-03-30 01:21:58 +0900

    modify js file

---
+++ js_kadai2.js
@@ -0,0 +1,53 @@
+function displayResult(){
+
+    const parentNode = document.getElementById(`display`);
+    //parentNode.innerHTML = `<div id=/"display/"></div>`;//display表示初期化
+    if(parentNode.getElementsByClassName(`children`).length > 0){
+      while (parentNode.firstChild){
+        parentNode.removeChild(parentNode.firstChild);
+      }
+    }
+  
+    const fizz = document.getElementById(`fizz`).value;
+    const buzz = document.getElementById(`buzz`).value;
+  
+    const regex = /^([1-9]\d*)$/;//整数の正規表現
+  
+    if(regex.test(fizz) && regex.test(buzz)){
+  
+      const fizzNum = parseInt(fizz);
+      const buzzNum = parseInt(buzz);
+  
+      for(let i=1,j=1; i<100,j<100;){
+        const pElement = document.createElement(`div`);
+        pElement.classList.add("children");
+  
+        const _fizzNum = fizzNum*i;
+        const _buzzNum = buzzNum*j;
+  
+        if(_fizzNum>=100 && _buzzNum>=100){
+          break;
+        }
+        if(_fizzNum < _buzzNum){
+          pElement.textContent = "Fizz " + _fizzNum;
+          i++;
+        }else if(_fizzNum > _buzzNum){
+          pElement.textContent = "Buzz " + _buzzNum;
+          j++;
+        }else{
+          pElement.textContent = "FizzBuzz " + _fizzNum;
+          i++;
+          j++;
+        }
+  
+        parentNode.appendChild(pElement);
+      }
+    }else{
+      const pElement = document.createElement(`div`);
+      pElement.classList.add("children");
+      pElement.textContent = "整数値を入力してください";
+      parentNode.appendChild(pElement);
+    }
+  
+  }
+  
