bfde3501eb3c61b5ed17940e83d0fafbfca8f607
yuki-manabi <hy.invest1001@gmail.com>
2021-04-01 12:38:44 +0900

    modify js

--- js_kadai2.js
+++ js_kadai2.js
@@ -1,24 +1,24 @@
-document.addEventListener(`DOMContentLoaded`, () => {
-  document.getElementById(`btn`).addEventListener(`click`, () => {
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('btn').addEventListener('click', () => {
 
-    const parentNode = document.getElementById(`display`);
-    //parentNode.innerHTML = `<div id=/"display/"></div>`;//display表示初期化
-    if (parentNode.getElementsByClassName(`children`).length > 0) {
+    const parentNode = document.getElementById('display');
+    //parentNode.innerHTML = '<div id=/"display/"></div>';//display表示初期化
+    if (parentNode.getElementsByClassName('children').length > 0) {
       while (parentNode.firstChild) {
         parentNode.removeChild(parentNode.firstChild);
       }
     }
   
-    const fizz = document.getElementById(`fizz`).value;
-    const buzz = document.getElementById(`buzz`).value;
+    const fizz = document.getElementById('fizz').value;
+    const buzz = document.getElementById('buzz').value;
   
     const regex = /^([1-9]\d*)$/;//整数の正規表現
   
     if (!regex.test(fizz) || !regex.test(buzz)) {
 
-      const pElement = document.createElement(`div`);
-      pElement.classList.add(`children`);
-      pElement.textContent = `整数値を入力してください`;
+      const pElement = document.createElement('div');
+      pElement.classList.add('children');
+      pElement.textContent = '整数値を入力してください';
       parentNode.appendChild(pElement);
       return;
 
@@ -27,8 +27,8 @@ document.addEventListener(`DOMContentLoaded`, () => {
     const buzzNum = parseInt(buzz);
 
     for (let i = 1; i < 100; i++) {
-      const pElement = document.createElement(`div`);
-      pElement.classList.add(`children`);
+      const pElement = document.createElement('div');
+      pElement.classList.add('children');
 
       if ((i % fizzNum === 0) && (i % buzzNum === 0)) {
         pElement.textContent = `FizzBuzz ${i}`;
